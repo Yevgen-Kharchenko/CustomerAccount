dc405c8ce21e663e0246ff07e702d12dc4f61d4d
Yevgen Kharchenko <yevgen.kharchenko.jd@gmail.com>
2019-12-30 14:17:43 +0200

    Initial commit

---
+++ src/main/java/com/example/CustomerAccount/CustomerAccountApplication.java
@@ -0,0 +1,14 @@
+package com.example.CustomerAccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAccountApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerAccountApplication.class, args);
+	}
+
+}
